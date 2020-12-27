3d96e879e2cf2d1e7ce4a9d38b08f63777e11448
Maulana0909 <72903410+Maulana0909@users.noreply.github.com>
2020-12-27 14:11:09 +0700

    Update and rename lib/main.dart to Lib/main.dart

--- Lib/main.dart
+++ Lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart;
-import 'package:apk_storage/
-splashscreen_view.dart;
+import 'package:apk_storage/splashscreen_view.dart;
 
 void main() => runApp(MyApp());
 class MyApp extends StatelessWigdet {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner:
-  false,
+      debugShowCheckedModeBanner: false,
       title: 'Splash Screen',
       home: Splashscreen(),
      );
