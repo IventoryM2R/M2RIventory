506593af2ea4297527fbefcb7ddea0e8ede872b0
Raju-Ramor <74889520+Raju-Ramor@users.noreply.github.com>
2020-12-29 19:03:36 +0700

    Update login.dart

--- Lib/login.dart
+++ Lib/login.dart
@@ -1,1 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String username = 'adudu.10gmail.com';
+  String password = 'adudu.10';
+  TextEditingController usernameController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+  final formKey = new GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height * 1,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(0),
+                child: new Image.network(
+                  'https://www.zahironline.com/wp-content/uploads/2017/03/icon-persediaan-barang-1.png',
+                  scale: 2,
+                ),
+              ),
+              _formBuilder(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _formBuilder() {
+    return Form(
+      key: formKey,
+      // autovalidate: true,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextFormField(
+              controller: usernameController,
+              keyboardType: TextInputType.emailAddress,
+              style: TextStyle(color: Colors.black),
+              validator: (value) {
+                return value.isEmpty ? "Username Tidak Boleh Kosong" : null;
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.pink, width: 10)),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1.5)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1.5)),
+                labelText: 'Username',
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextFormField(
+              controller: passwordController,
+              obscureText: true,
+              style: TextStyle(color: Colors.black),
+              validator: (value) {
+                return value.isEmpty ? "Password Tidak Boleh Kosong" : null;
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black)),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 1.5),
+                ),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1.5)),
+                labelText: 'Password',
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+
+          //BUTTON
+          RaisedButton(
+            onPressed: () {
+              if (formKey.currentState.validate() &&
+                  usernameController.text.toString() == username) {
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                    (Route<dynamic> route) => false);
+              }
+            },
+            color: Colors.pink,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.07,
+              child: Center(
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontSize: 24, color: Colors.black),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
